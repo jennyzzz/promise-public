19970ded65d74d3a3987b0b6cfad9fc596c6f93e
Your Name <you@example.com>
2018-10-14 17:15:12 +0800

    'promise-all'

--- case2.js
+++ case2.js
@@ -1,37 +1,27 @@
 let fs = require('fs');
-
-function read(url, encoding) {
-  return new Promise(function (resolve, reject) {
-    fs.readFile(url, encoding, function (err, data) {
-      //  promise 的成功或者失败 取决于你是怎样定义的
-      if (err) reject(err);
-      resolve(data)
-    });
-  });
-}
-// Promise.all 表示全部成功才成功 有任意一个失败 都会失败
-Promise.all = function (promises) {
-  return new Promise((resolve,reject)=>{
-    let arr = [];
-    let currentIndex = 0;
-    function processData(index,val) {
-      arr[index] = val;
-      currentIndex++; // 记录一下成功的次数
-      // 如果达到了执行目标就让all的promise成功
-      if(currentIndex === promises.length){
-        resolve(arr);
-      }
+//let bluebird = require('bluebird');// node已经吸收过来了
+let bluebird = {
+  promisify(fn){
+    return function (...args) {
+      return new Promise((resolve,reject)=>{
+        // node异步方法最后一个参数都是回调函数
+        fn(...args,function (err,data) {
+          if(err)reject(err);
+          resolve(data);
+        });
+      })
     }
-    for(let i = 0; i<promises.length;i++){
-      promises[i].then(function (data) {
-        processData(i, data)
-      },reject)
+  },
+  promisifyAll(obj){
+    for(let key in obj ){
+      obj[key+'Async'] = this.promisify(obj[key]);
     }
-  })
+  }
 }
-Promise.all([read('name.txt', 'utf8'), read('age.txt', 'utf8')]).then(data=>{
+// let read = bluebird.promisify(fs.readFile);
+bluebird.promisifyAll(fs);
+fs.readFileAsync('name.txt','utf8').then(data=>{
   console.log(data);
-},err=>{
-  console.log(err);
 })
+// 我们可以封装promise 别人也可以封装 Q bluebird
 
--- promise.js
+++ promise.js
@@ -155,6 +155,34 @@ Promise.prototype.finally = function(callback){
     })
   })
 }
+// Promise.all 表示全部成功才成功 有任意一个失败 都会失败
+Promise.all = function (promises) {
+  return new Promise((resolve, reject) => {
+    let arr = [];
+    let currentIndex = 0;
+    function processData(index, val) {
+      arr[index] = val;
+      currentIndex++; // 记录一下成功的次数
+      // 如果达到了执行目标就让all的promise成功
+      if (currentIndex === promises.length) {
+        resolve(arr);
+      }
+    }
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then(function (data) {
+        processData(i, data);
+      }, reject)
+    }
+  });
+}
+// rece赛跑
+Promise.race = function (promises) {
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then(resolve, reject);
+    }
+  });
+}
 // Promise.prototype.finally = function (callback) {
 //   let P = this.constructor;
 //   return this.then(
