00cd602fc4dc1410c9f771a4e0775978282c5ef7
Your Name <you@example.com>
2018-10-14 11:33:03 +0800

    'promise-base'

--- 2.promise/1.promise.js
+++ 2.promise/1.promise.js
@@ -3,28 +3,18 @@ function Promise(executor) {
   // 保存成功的值和失败的原因
   self.value = undefined;
   self.reason = undefined;
-  // 专门存成功的回调
-  self.onResolvedCallbacks = [];
-  // 专门存失败的回调
-  self.onRejectedCallbacks = [];
   // 保存一下当前这个promise的状态(promise有三个状态)
   self.status = 'pending';
   function resolve(value) {
     if (self.status === 'pending') {
       self.value = value;
       self.status = 'resolved';
-      self.onResolvedCallbacks.forEach(function (fn) {
-        fn();
-      });
     }
   }
   function reject(reason) {
     if (self.status === 'pending') {
       self.reason = reason;
       self.status = 'rejected';
-      self.onRejectedCallbacks.forEach(function (fn) {
-        fn();
-      })
     }
   }
   executor(resolve, reject);
@@ -38,15 +28,6 @@ Promise.prototype.then = function (onFulfilled,onRejected) {
   }
   if (self.status === 'rejected') {
     onRejected(self.reason);
-  }
-  // executor中有异步操作，此时调用then时 处于等待态
-  if(self.status === 'pending'){
-    self.onResolvedCallbacks.push(function(){
-      onFulfilled(self.value)
-    });
-    self.onRejectedCallbacks.push(function () {
-      onRejected(self.reason);
-    })
   }
 }
 module.exports = Promise
