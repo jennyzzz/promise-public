a367c3e33d4ed8f651fc77096d26225bd90da313
Jenny Zhu <jenny.zhenzheng@gmail.com>
2018-10-16 13:32:51 -0700

    Update 1.promise.js

--- 2.promise/1.promise.js
+++ 2.promise/1.promise.js
@@ -1,3 +1,6 @@
+// source code Part I
+// tests are in 1.case.js
+
 function Promise(executor) {
   let self = this;
   // 保存成功的值和失败的原因
@@ -5,21 +8,26 @@ function Promise(executor) {
   self.reason = undefined;
   // 保存一下当前这个promise的状态(promise有三个状态)
   self.status = 'pending';
+  
   function resolve(value) {
     if (self.status === 'pending') {
       self.value = value;
       self.status = 'resolved';
     }
   }
+  
   function reject(reason) {
     if (self.status === 'pending') {
       self.reason = reason;
       self.status = 'rejected';
     }
   }
+  
   executor(resolve, reject);
   // executor是立即执行的
 }
+
+
 // then方法中需要传递两个参数 分别是成功的回调和失败的回调
 Promise.prototype.then = function (onFulfilled,onRejected) {
   let self = this;
@@ -31,3 +39,4 @@ Promise.prototype.then = function (onFulfilled,onRejected) {
   }
 }
 module.exports = Promise
+
