b7c156c26fba934b30c2855501861f559f7b2fc7
Jenny Zhu <jenny.zhenzheng@gmail.com>
2018-10-16 13:50:44 -0700

    Update 2.case.js

--- 2.promise/2.case.js
+++ 2.promise/2.case.js
@@ -1,6 +1,6 @@
 // if using './1.promise', it couldn't handle the asych case.
-// Promise from 1.promise would handle .then before setTimeout is due.
-// So, we need './2.promise', using subscribe-publish to save the .then in an array first,
+// Promise from 1.promise would handle callback before setTimeout is due.
+// So, we need './2.promise', using subscribe-publish to save the callbacks in an array first,
 // then to execute .then after setTimeout is done.
 
 let Promise = require('./1.promise');
